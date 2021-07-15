fc4503e84a3d00c6708c1402761d4003a94bf4ea
Haruka39 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-07-15 13:26:38 +0700

    Rollback

--- lib/commands/dev/reset.py
+++ lib/commands/dev/reset.py
@@ -4,20 +4,8 @@ from lib.settings import *
 
 
 @bot.command()
-async def reset(cmd, user: discord.User = None):
+async def reset(cmd, user: discord.User):
     if await bot.is_owner(cmd.author):
-        if user is None:
-            cur.execute(f"""
-            DELETE FROM economy;
-            """)
-            conn.commit()
-            lst = [300, None, 0, 1.01]
-            for i in range(54):
-                lst.append(0)
-            for id in data.keys():
-                data[id] = lst
-            await cmd.send(embed=discord.Embed(title="Request accepted", description=f"Successfully removed all from database", color=0x2ECC71))
-            return
         id = str(user.id)
         try:
             data[id]
