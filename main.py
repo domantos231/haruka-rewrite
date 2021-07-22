dd47db67792d89bf4f516f308018293e2989d5de
Haruka39 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-07-22 21:54:29 +0700

    Closed bot session after all loops

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from lib.settings import bot, TOKEN, conn, cur
+import asyncio
+from lib.settings import bot, TOKEN, session
 from lib import *
 
 
-bot.run(TOKEN)
+async def cancel(session):
+    await session.close()
+    print("Side session closed.")
+
+
+try:
+    bot.loop.run_until_complete(bot.start(TOKEN))
+except:
+    bot.loop.run_until_complete(bot.close())
+finally:
+    bot.loop.close()
+    asyncio.run(cancel(session))
