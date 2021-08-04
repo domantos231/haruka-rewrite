f0159b84c90de977feeb40683dd9c1ddefc1a606
Haruka39 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-08-04 16:09:00 +0700

    Changed message context

--- lib/commands/gif/girl.py
+++ lib/commands/gif/girl.py
@@ -7,6 +7,6 @@ from settings import *
 @bot.command(name="girl")
 async def _girl(cmd):
     gifs = await GIF("anime-girl").giphy_leech()
-    em = discord.Embed(description=f"**{cmd.author.name}** met a girl today!", color=0x2ECC71)
+    em = discord.Embed(description=f"**{cmd.author.name}**, this is your girl!", color=0x2ECC71)
     em.set_image(url=choice(gifs))
     await cmd.send(embed=em)
