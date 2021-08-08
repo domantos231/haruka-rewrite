4f0ac80ab938648146c2003e440f105bcfe12c53
Haruka39 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-08-08 17:33:06 +0700

    Fixed bug

--- lib/events/on_guild_join.py
+++ lib/events/on_guild_join.py
@@ -4,7 +4,7 @@ from settings import *
 @bot.event
 async def on_guild_join(guild):
     id = guild.id
-    row = bot.db.conn.fetchrow(f"SELECT id FROM prefix WHERE id = '{id}';")
+    row = await bot.db.conn.fetchrow(f"SELECT id FROM prefix WHERE id = '{id}';")
     if not row:
         await bot.db.conn.execute(f"""
         INSERT INTO prefix
