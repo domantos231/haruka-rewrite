3040d9f087881741fe735689522d17341eb8d33f
Haruka39 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-08-11 00:11:05 +0700

    Stack all developer commands

---
+++ lib/commands/dev/exec.py
@@ -0,0 +1,21 @@
+import discord
+from settings import *
+
+
+@bot.command(name="exec")
+async def _exec(cmd, *func):
+    if not await bot.is_owner(cmd.author):
+        await cmd.send("This command is available for developers only.")
+        return
+    display = ""
+    for f in func:
+        if f.startswith("await"):
+            try:
+                await eval(f)
+            except Exception as ex:
+                display += f"Exception at {f}: {ex}\n"
+        else:
+            try:
+                val = eval(f)
+            except Exception as ex:
+                display += f"Exception at {f}: {ex}\n"
