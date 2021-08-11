e5c83db1d6204196a7f660167769d09da39783b3
Haruka39 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-08-11 17:49:37 +0700

    Added command to get log file

---
+++ lib/commands/dev/log.py
@@ -0,0 +1,8 @@
+import discord
+
+
+@bot.command(name="log")
+async def _log(cmd):
+    if not await bot.is_owner(cmd.author):
+        return await cmd.send("This command is available for developers only.")
+    await cmd.send(content="This is the log file", file=discord.File("./log.txt"))
