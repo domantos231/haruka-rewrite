2a33812fad795ca5560d02aaeb1663c262e6ec7d
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-08-22 09:27:52 +0700

    Disconnect player when websocket is closed

--- lib/events/wavelink.py
+++ lib/events/wavelink.py
@@ -10,3 +10,9 @@ async def on_wavelink_node_ready(node):
 async def on_wavelink_track_exception(player, track, error):
     print(f"HARUKA | TrackException in server '{player.guild}' while playing '{track.title}': {error}")
     await player.disconnect(force=True)
+
+
+@bot.event
+async def on_wavelink_websocket_closed(player, reason, code):
+    print(f"HARUKA | WebsocketClosed in server '{player.guild}' while playing '{track.title}' ({reason}): {code}")
+    await player.disconnect(force=True)
