aa4264d7844ff473c7a9bccd602f32ca935a9f68
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-08-22 23:32:16 +0700

    Added annotation in message content

--- lib/events/on_command_error.py
+++ lib/events/on_command_error.py
@@ -34,7 +34,7 @@ async def on_command_error(cmd, error):
     elif isinstance(error, commands.UserInputError):
         await cmd.send("📜 Please check your input again.")
     elif isinstance(error, commands.CommandInvokeError):
-        await cmd.send(f"🔧 An error occurred:\n```\n{error.original}\n```")
+        await cmd.send(f"🔧 An error occurred:\n```\n{error.original}\n```\nIf this is a bug, hopefully it will be removed in the future 😉")
         print(f"HARUKA | '{cmd.message.content}' in {cmd.guild}/{cmd.channel} {type(error)} {error}")
     else:
         print(f"HARUKA | '{cmd.message.content}' in {cmd.guild}/{cmd.channel} {type(error)} {error}")
