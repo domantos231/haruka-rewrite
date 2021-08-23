8ef33a74ffbea98866b1542dd58396ee6ba9e0ad
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-08-23 21:58:43 +0700

    Fixed bug

--- lib/commands/msc/stop.py
+++ lib/commands/msc/stop.py
@@ -5,7 +5,7 @@ from settings import *
 @commands.cooldown(1, 5, commands.BucketType.guild)
 async def _stop(cmd):
     player = bot.node.get_player(cmd.guild)
-    if player.is_connected:
+    if player and player.is_connected:
         await player.stop()
         await player.disconnect(force=True)
         await cmd.send("Stopped player.")
