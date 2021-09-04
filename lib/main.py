47ede23a2ee32260be61fb80e3d644fe4c0fb870
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-09-04 07:52:01 +0700

    Enabled reporting upon ready

--- lib/main.py
+++ lib/main.py
@@ -1,5 +1,4 @@
-﻿import asyncio
-import traceback
+﻿import discord
 
 
 if __name__ == "__main__":
@@ -15,6 +14,8 @@ if __name__ == "__main__":
 
     @bot.event
     async def on_ready():
+        file = discord.File("./log.txt")
+        await bot.get_user(ME).send("Bot has just started. This is the report.", file = file)
         print(f"HARUKA | Logged in as {bot.user} | Running in {len(bot.guilds)} servers.")
 
 
