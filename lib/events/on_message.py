59ee484d565acf381ab549fafe81da30f59ea1ed
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-09-04 23:22:41 +0700

    Fixed bug

--- lib/events/on_message.py
+++ lib/events/on_message.py
@@ -6,16 +6,6 @@ from objects import *
 async def on_message(message):
     if message.author.bot:
         return
-    if bot.user.mentioned_in(message):
-        if isinstance(message.channel, discord.TextChannel):
-            id = message.guild.id
-            row = await bot.db.conn.fetchrow(f"SELECT * FROM prefix WHERE id = '{id}';")
-            pref = row["pref"]
-            await message.channel.send(f"My current prefix is: {pref}")
-        elif isinstance(message.channel, discord.DMChannel):
-            await message.channel.send("My current prefix is: $")
-        else:
-            await cmd.send("This channel type is not supported.")
     if isinstance(message.channel, discord.TextChannel):
         try:
             id = message.guild.id
@@ -28,5 +18,15 @@ async def on_message(message):
                 INSERT INTO prefix (id, pref)
                 VALUES ('{id}', '$');
                 """)
+    if bot.user.mentioned_in(message):
+        if isinstance(message.channel, discord.TextChannel):
+            id = message.guild.id
+            row = await bot.db.conn.fetchrow(f"SELECT * FROM prefix WHERE id = '{id}';")
+            pref = row["pref"]
+            await message.channel.send(f"My current prefix is: {pref}")
+        elif isinstance(message.channel, discord.DMChannel):
+            await message.channel.send("My current prefix is: $")
+        else:
+            await cmd.send("This channel type is not supported.")
     if isinstance(message.channel, discord.TextChannel) or isinstance(message.channel, discord.DMChannel):
         await bot.process_commands(message)
