360680951f729a04d31616bbfea8b792e42ee3b2
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2021-09-05 12:31:42 +0700

    Improved avatar fetching method

--- lib/commands/slash/avatar.py
+++ lib/commands/slash/avatar.py
@@ -18,15 +18,16 @@ json = {
 @bot.slash(json)
 async def avatar_(interaction):
     response = interaction.response
-    user_id = int(list(interaction.data["resolved"]["users"].keys())[0])
-    user = bot.get_user(user_id)
-    if user:
-        embed = discord.Embed(color = 0x2ECC71)
-        embed.set_author(
-            name = f"This is {user.name}'s avatar",
-            icon_url = bot.user.avatar.url,
-        )
-        embed.set_image(url = user.avatar.url)
-        await response.send_message(embed = embed)
-    else:
-        await response.send_message(content = "Cannot find this user.")
+    users = interaction.data["resolved"]["users"]
+    user_id = list(users.keys())[0]
+
+    avatar_hash = users[user_id]["avatar"]
+    user_name = users[user_id]["username"]
+
+    embed = discord.Embed(color = 0x2ECC71)
+    embed.set_author(
+        name = f"This is {user_name}'s avatar",
+        icon_url = bot.user.avatar.url,
+    )
+    embed.set_image(url = f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_hash}.png?size=1024")
+    await response.send_message(embed = embed)
